8001313212b28b1d7d3538ff7c04de940942ed9a
mcmero <7799828+mcmero@users.noreply.github.com>
2024-12-09 15:43:30 +1100

    Combine channel functionality

--- src/mesmer_segment/__init__.py
+++ src/mesmer_segment/__init__.py
@@ -1,9 +1,9 @@
 import typer
 from pathlib import Path
 from tifffile import TiffFile, imwrite
-from xarray import DataArray
+from xarray import DataArray, concat
 import json
-from typing import Annotated
+from typing import Annotated, List
 from numpy.typing import NDArray
 import sys
 
@@ -29,21 +29,47 @@ def mibi_tiff_to_xarray(tiff: TiffFile) -> DataArray:
 
     return DataArray(data=channels, dims=["C", "Y", "X"], coords={"C": channel_names}, attrs=attrs)
 
+
+def combine_channels(array: DataArray, channels: List[str], combined_name: str) -> DataArray:
+    """
+    Combines multiple channels into a single channel by taking the maximum value at each pixel.
+    Adds the combined channel to the array.
+    """
+
+    if len(channels) == 1:
+        return array
+
+    combined = array.sel(
+        C=channels
+    ).max(
+        dim="C"
+    ).expand_dims(
+        "C"
+    ).assign_coords(
+        C=[combined_name]
+    )
+
+    return concat([array, combined], dim="C")
+
 @app.command(help="Segments a MIBI TIFF using Mesmer, and prints the result to stdout. Note that you will need to obtain and export a DeepCell API key as explained [here](https://deepcell.readthedocs.io/en/master/API-key.html).")
 def main(
     mibi_tiff: Annotated[Path, typer.Argument(help="Path to the MIBI TIFF input file")],
     nuclear_channel: Annotated[str, typer.Option(help="Name of the nuclear channel")],
-    membrane_channel: Annotated[str, typer.Option(help="Name of the membrane channel")]
+    membrane_channel: Annotated[List[str], typer.Option(help="Name(s) of the membrane channels (can be repeated)")],
 ):
     from deepcell.applications import Mesmer
+    from deepcell.utils.plot_utils import create_rgb_image, make_outline_overlay
+
     tiff = TiffFile(mibi_tiff)
     array = mibi_tiff_to_xarray(tiff)
+    array = combine_channels(array, membrane_channel, "combined_membrane")
 
     # Mesmer assumes the input is:
     # A 4D array with dimensions (batch, x, y, channel)
     # There must be exactly 2 channels, and they have to correspond to nuclear and channel markers respectively
+    combined_membrane_channel = "combined_membrane" if len(membrane_channel) > 1 else membrane_channel[0]
     np_array = array.sel(
-        C=[nuclear_channel, membrane_channel]
+        C=[nuclear_channel, combined_membrane_channel]
     ).expand_dims(
         "batch"
     ).transpose(
