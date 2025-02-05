3a9d42333e2df9e5123efcefc02ed688bd7da84a
mcmero <7799828+mcmero@users.noreply.github.com>
2025-02-05 16:31:36 +1100

    Add maxima_smooth, min_nuclei area params and support for padded images

--- src/mesmer_segment/__init__.py
+++ src/mesmer_segment/__init__.py
@@ -14,6 +14,7 @@ from tifffile import TiffFile
 from xarray import DataArray, concat
 from deepcell.applications import Mesmer
 from skimage.segmentation import clear_border
+from skimage.util import crop
 
 app = typer.Typer(rich_markup_mode="markdown")
 MISSING = object()
@@ -59,7 +60,7 @@ def combine_channels(array: DataArray, channels: List[str], combined_name: str, 
         combined = combined.max(dim="C")
     elif combine_method == CombineMethod.PROD:
         combined = combined.prod(dim="C")
-    
+
     combined = combined.expand_dims("C").assign_coords(C=[combined_name])
 
     return concat([array, combined], dim="C")
@@ -80,7 +81,7 @@ def calculate_maxima_threshold(segmentation_level: int) -> float:
     return 0.1 - 0.1 * subtractive_factor
 
 
-def get_segmentation_predictions(array: DataArray, nuclear_channel: str, membrane_channel: List[str], kwargs_nuclear: dict[str, float], kwargs_whole_cell: dict[str, float]) -> NDArray:
+def get_segmentation_predictions(array: DataArray, nuclear_channel: str, membrane_channel: List[str], kwargs_nuclear: dict[str, float], kwargs_whole_cell: dict[str, float], padding: int) -> NDArray:
     """
     Segments the input array using Mesmer.
     Mesmer assumes the input is a 4D array with dimensions (batch, x, y, channel).
@@ -95,12 +96,16 @@ def get_segmentation_predictions(array: DataArray, nuclear_channel: str, membran
         "batch", "X", "Y", "C"
     ).to_numpy()
 
-    app = Mesmer()
-    mpp = array.attrs["fov_size"] / array.attrs["frame_size"]
+    # Crop the image if padding is specified
+    if padding > 0:
+        np_array = crop(np_array, ((0, 0), (padding, padding), (padding, padding), (0, 0)))
 
     # Set compartment to nuclear if using pixel expansion
     assert 'pixel_expansion' in kwargs_nuclear, "pixel_expansion must be specified in kwargs_nuclear"
     compartment = "nuclear" if kwargs_nuclear['pixel_expansion'] > 0 else "whole-cell"
+
+    app = Mesmer()
+    mpp = array.attrs["fov_size"] / array.attrs["frame_size"]
 
     # The result is a 4D array, but the first and last dimensions are both 1
     return app.predict(
@@ -142,6 +147,7 @@ def labels_to_features(lab: np.ndarray, object_type="annotation", connectivity: 
 
     return features
 
+
 @app.command(help="Segments a MIBI TIFF using Mesmer, and prints the result to stdout. Note that you will need to obtain and export a DeepCell API key as explained [here](https://deepcell.readthedocs.io/en/master/API-key.html).")
 def main(
     mibi_tiff: Annotated[Path, typer.Argument(help="Path to the MIBI TIFF input file")],
@@ -150,8 +156,11 @@ def main(
     combine_method: Annotated[CombineMethod, typer.Option(help="Method to use for combining channels (prod or max)")] = CombineMethod.PROD,
     segmentation_level: Annotated[int, typer.Option(help="Segmentation level between 0-10 where 0 is less segmentation and 10 is more", min=0, max=10)] = 5,
     interior_threshold: Annotated[float, typer.Option(help="Controls how conservative model is in distinguishing cell from background (lower values = larger cells, higher values = smaller cells)")] = 0.3,
+    maxima_smooth: Annotated[float, typer.Option(help="Controls what is considered a unique cell (lower values = more separate cells, higher values = fewer cells)", min=0)] = 0,
+    min_nuclei_area: Annotated[int, typer.Option(help="Minimum area of nuclei to keep", min=0)] = 15,
     remove_cells_touching_border: Annotated[bool, typer.Option(help="Whether to remove cells touching the border of the image")] = True,
     pixel_expansion: Annotated[int, typer.Option(help="Specify a manual pixel expansion after segmentation. NOTE: This will perform segmentation in nuclear mode only.")] = 0,
+    padding: Annotated[int, typer.Option(help="Number of pixels to crop the image by before segmentation", min=0)] = 96,
 ):
 
     tiff = TiffFile(mibi_tiff)
@@ -162,11 +171,14 @@ def main(
     maxima_threshold = calculate_maxima_threshold(segmentation_level)
     kwargs_nuclear = {'pixel_expansion': pixel_expansion,
                       'maxima_threshold': maxima_threshold,
-                      'interior_threshold': interior_threshold}
+                      'maxima_smooth': maxima_smooth,
+                      'interior_threshold': interior_threshold,
+                      'small_objects_threshold': min_nuclei_area}
     kwargs_whole_cell = {'maxima_threshold': maxima_threshold,
+                         'maxima_smooth': maxima_smooth,
                          'interior_threshold': interior_threshold}
     segmentation_predictions = get_segmentation_predictions(array, nuclear_channel, membrane_channel,
-                                                            kwargs_nuclear, kwargs_whole_cell)
+                                                            kwargs_nuclear, kwargs_whole_cell, padding)
 
     # Post processing functions
     if remove_cells_touching_border:
@@ -179,6 +191,11 @@ def main(
     geoms = []
     for feature in features:
         geoms.append(feature["geometry"])
+
+    # Adjust coordinates to account for cropping
+    if padding > 0:
+        for geom in geoms:
+            geom["coordinates"] = [[[x+padding, y+padding] for x, y in geom["coordinates"][0]]]
 
     # Create a geopandas dataframe from the geometries and properties
     gdf = gpd.GeoDataFrame.from_features(features)
